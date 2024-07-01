238457e0568f220bbfb8e1947e8e7000d9404942
Montie1226 <houeymt@gmail.com>
2024-07-01 04:01:03 -0500

    console.log

---
+++ index.js
@@ -0,0 +1,18 @@
+// Create a string for the user saying: you have received this message beacause you have been chosen to open an important Vault.
+const message = "You have received this message beacause you have been chosen to open an important Vault! Here is the secret code combination:"
+
+
+// assign three different variables for the code
+
+const codeOne = 2*5;
+const codeTwo = 80-40;
+const codeThree = 78/2;
+
+console.log(codeOne);
+console.log(codeTwo);
+console.log(codeThree);
+
+
+// create a popup dialog box displaying the vault codes and the text
+
+alert('The Vault Code Is 10-40-39');
